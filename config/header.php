a6b9a5325b489d896ca9be3a09af34a6b05582a1
Hadi Prasetia <68311980+hs-1337@users.noreply.github.com>
2020-07-15 10:02:24 +0700

    Add files via upload

---
+++ config/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="SHORTCUT ICON" href="https://qph.fs.quoracdn.net/main-qimg-aa183f8cd1dbd1d135a36f34f0ab03b5" />
+    <link rel="apple-touch-icon" href="https://qph.fs.quoracdn.net/main-qimg-aa183f8cd1dbd1d135a36f34f0ab03b5" sizes="180x180" />
+    <link rel="icon" href="https://qph.fs.quoracdn.net/main-qimg-aa183f8cd1dbd1d135a36f34f0ab03b5" sizes="32x32" type="image/png" />
+    <link rel="icon" href="https://qph.fs.quoracdn.net/main-qimg-aa183f8cd1dbd1d135a36f34f0ab03b5" sizes="16x16" type="image/png" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="./config/style.css" />
+    <?php error_reporting(0); ?>
+    <meta name="description" content="jadwal mata pelajaran kelas 9B">
+    <title>JADWAL 9B - <?= $title; ?></title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark sticky-top">
+        <div class="container">
+            <div class="container">
+                <a class="navbar-brand hs-1" href="#">JADWAL 9B</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto">
+                        <a class="nav-item nav-link hs-ver" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        <!-- <a class="nav-item nav-link hs-ver" href="javascript:(0, 0, 2);">About</a>
+                        <a class="nav-item nav-link hs-ver" href="javascript:(0, 0, 3);">Gallery</a>
+                        <a class="nav-item nav-link hs-ver" href="javascript:(0, 0, 4);">Activity</a>
+                        <a class="nav-item nav-link hs-ver disable" href="javascript:(0, 0, 5);">Library</a> -->
+                        <a class="nav-item nav-link hs-ver" href="info.php">Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
